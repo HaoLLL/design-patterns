55aa674ffd131c3611cc7013c0dd9129d6f7f04d
郝雷 <m13522389185@163.com>
2021-02-16 15:13:10 +0800

    bridge DP

--- src/com/company/bridge/Bridge.java
+++ src/com/company/bridge/Bridge.java
@@ -1,34 +1,50 @@
 package com.company.bridge;
 
+
 public class Bridge {
     public static void main(String[] args) {
-        Rec r = new Rec();
-        White w = new White();
-        r.setColor(w);
-        r.draw();
+        DrawAPI red = new RedCircle();
 
     }
 }
 
+// 1. 桥梁 展开成好几种实现类
+// 2. 构造或set中设置实现类 方法中执行桥梁方法
+interface DrawAPI{
+    public void drawCircle();
+}
+
+
+class GreenCircle implements DrawAPI{
+    @Override
+    public void drawCircle() {
+        System.out.println("green draw");
+    }
+}
+
+class RedCircle implements DrawAPI{
+    @Override
+    public void drawCircle() {
+        System.out.println("red draw");
+    }
+}
 abstract class Shape{
-    Color color;
-    public void setColor(Color color){
-        this.color = color;
+    DrawAPI drawAPI;
+    public Shape(DrawAPI drawAPI){
+        this.drawAPI = drawAPI;
     }
-    abstract void draw();
+    public abstract  void draw();
 }
-interface Color{
-    public void print();
-}
-class White implements Color{
-    @Override
-    public void print() {
-        System.out.println("print white");
+class Circle extends Shape {
+
+    public Circle(DrawAPI drawAPI) {
+        super(drawAPI);
     }
-}
-class Rec extends Shape{
-    @Override
-    void draw() {
-        color.print();
+
+    public void draw() {
+        drawAPI.drawCircle();
     }
 }
+
+
+
---
+++ src/com/company/factory/abstractFactory.java
@@ -0,0 +1,16 @@
+package com.company.factory;
+
+// 大工厂 加盟商需要特定的工厂方法
+public class abstractFactory {
+    public static void main(String[] args) {
+
+    }
+}
+class White
+
+
+abstract class AbstractFactory{
+    abstract Color getColor(String color);
+    abstract Shape getShape(String shape);
+}
+
