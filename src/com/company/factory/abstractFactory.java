52f0d0994a9585475759136fef2ddcd245012b38
郝雷 <m13522389185@163.com>
2021-02-18 15:32:08 +0800

    abstract factory

--- src/com/company/factory/abstractFactory.java
+++ src/com/company/factory/abstractFactory.java
@@ -1,5 +1,71 @@
 package com.company.factory;
 
-//package com.company.factory;
-//
-//// 大工厂 加盟商需要特定的工厂方法
+//工厂方法：animalFarm只是produce Animal
+//养动物 + 养植物
+//综合性工厂
+// abstract factory
+// concrete factory
+// abstract product
+// concrete product
+
+public class abstractFactory{
+    public static void main(String[] args) {
+        Farm2 f2 = new ConcreteFarm2();
+        Animal2 a2 = f2.produceAnimal();
+        Plant2 p2 = f2.productPlant();
+        a2.show();
+        p2.show();
+
+    }
+
+}
+
+interface Animal2{
+    void show();
+}
+class Horse2 implements Animal2{
+    @Override
+    public void show() {
+        System.out.println("Horse show");
+    }
+}
+
+class Cattle2 implements Animal2{
+    @Override
+    public void show() {
+        System.out.println("Cattle show");
+    }
+}
+
+
+interface Plant2{void show();}
+class Flower2 implements Plant2{
+    @Override
+    public void show() {
+        System.out.println("Flower show");
+    }
+}
+class Grass2 implements Plant2{
+    @Override
+    public void show() {
+        System.out.println("Grass show");
+    }
+}
+
+interface Farm2{
+    Animal2 produceAnimal();
+    Plant2 productPlant();
+}
+class ConcreteFarm2 implements Farm2{
+    @Override
+    public Animal2 produceAnimal() {
+        return new Horse2();
+    }
+
+    @Override
+    public Plant2 productPlant() {
+        return new Grass2();
+    }
+}
+
+
